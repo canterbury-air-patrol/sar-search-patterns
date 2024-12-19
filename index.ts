f39802cc5c4e6124ed6eb947058ce56dd5519cfb
Scott Parlane <scott@parlanenz.com>
2024-12-19 22:56:39 +1300

    Create an index.ts to export all classes

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { SearchConfiguration, SearchDisplay } from './react'
+import { SearchPattern, SearchLeg, SectorSearch, ExpandingBoxSearch, CreepingLineAheadSearch } from './sar-search-patterns'
+
+export { SearchConfiguration, SearchDisplay, SearchPattern, SearchLeg, SectorSearch, ExpandingBoxSearch, CreepingLineAheadSearch }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['sar-search-patterns.ts', 'react.tsx'],
+  entry: ['index.ts'],
   format: ['cjs', 'esm'], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
